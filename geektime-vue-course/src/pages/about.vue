fd488bc67732a80fa4e3b3dafd888e6bc1150691
lhr <317424034@qq.com>
2021-12-18 18:24:16 +0800

    VueX使用
    1. 安装VueX
    2.定义VueX的Store对象
    3.注册Store对象
    4.使用Store对象

--- geektime-vue-course/src/pages/about.vue
+++ geektime-vue-course/src/pages/about.vue
@@ -2,7 +2,9 @@
 <template>
     <h1>这是关于页面</h1>
     <HelloWorld/>
+    <Count></Count>
 </template>
 <script setup>
 import HelloWorld from '../components/HelloWorld.vue'
+import Count from '../components/Count.vue'
 </script>
---
+++ geektime-vue-course/src/store/index.js
@@ -0,0 +1,14 @@
+
+import { createStore } from 'vuex'
+export default createStore({
+  state () {
+    return {
+      count: 666
+    }
+  },
+  mutations: {
+    add (state) {
+      state.count++
+    }
+  }
+})
